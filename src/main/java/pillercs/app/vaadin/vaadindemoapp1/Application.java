0264bb05c5dc2984ad789e2e5f1e9522ecc07ec7
Csaba Piller <vqaaih@inf.elte.hu>
2022-09-25 18:46:16 +0200

    first commit

---
+++ src/main/java/pillercs/app/vaadin/vaadindemoapp1/Application.java
@@ -0,0 +1,29 @@
+package pillercs.app.vaadin.vaadindemoapp1;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/abel", version = "4.5.0")
+@Theme(value = "vaadindemoapp1", variant = Lumo.DARK)
+@PWA(name = "Vaadin demo app 1", shortName = "Vaadin demo app 1", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
